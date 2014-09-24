6023892559cbaf3b2d4615637dc5ba65ffe49db6
UrbanBjorkman <urban.bjorkman@gmail.com>
2014-09-24 10:53:46 +0200

    Added code

---
+++ SimpleTimer/SimpleTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleTimer
+{
+	public static class SimpleTimer
+	{
+		public static IDisposable SetInterval(Action method, int delayInMilliseconds)
+		{
+			var timer = new System.Timers.Timer(delayInMilliseconds);
+			timer.Elapsed += (source, e) => method();
+
+			timer.Enabled = true;
+			timer.Start();
+
+			// Returns a stop handle which can be used for stopping
+			// the timer, if required
+			return timer as IDisposable;
+		}
+
+		public static IDisposable SetTimeout(Action method, int delayInMilliseconds)
+		{
+			var timer = new System.Timers.Timer(delayInMilliseconds);
+			timer.Elapsed += (source, e) => method();
+
+			timer.AutoReset = false;
+			timer.Enabled = true;
+			timer.Start();
+
+			// Returns a stop handle which can be used for stopping
+			// the timer, if required
+			return timer as IDisposable;
+		}
+	}
+}
